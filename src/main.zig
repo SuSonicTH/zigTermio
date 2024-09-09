14e5e3bfa8adf44ec708587824dd5f0df4203a59
Michael Wolf <susonicth@gmail.com>
2024-09-09 18:25:42 +0200

    implemented get/set cursor for windows

--- src/main.zig
+++ src/main.zig
@@ -2,5 +2,12 @@ const std = @import("std");
 const Termio = @import("Termio.zig");
 
 pub fn main() !void {
-    _ = try std.io.getStdOut().writer().print("{any}", .{Termio.getTerminalSize()});
+    const size = try Termio.getTerminalSize();
+    _ = try std.io.getStdOut().writer().print("{any}\n", .{size});
+    _ = try std.io.getStdOut().writer().print("{any}\n", .{Termio.getCursor()});
+    try Termio.setCursor(.{});
+    _ = try std.io.getStdOut().writer().print("    CURSOR SET    ", .{});
+
+    try Termio.setCursor(.{ .y = size.height - 1 });
+    _ = try std.io.getStdOut().writer().print("{any}\n", .{Termio.getCursor()});
 }
--- src/termio.zig
+++ src/termio.zig
@@ -8,14 +8,11 @@ pub const TerminalSize = struct {
 
 pub fn getTerminalSize() !TerminalSize {
     if (builtin.os.tag == .windows) {
-        var buffer: std.os.windows.CONSOLE_SCREEN_BUFFER_INFO = undefined;
-
-        if (std.os.windows.kernel32.GetConsoleScreenBufferInfo(std.io.getStdOut().handle, &buffer) == std.os.windows.TRUE) {
-            return .{
-                .width = @intCast(buffer.srWindow.Right - buffer.srWindow.Left + 1),
-                .height = @intCast(buffer.srWindow.Bottom - buffer.srWindow.Top + 1),
-            };
-        }
+        const info = try getConsoleScreenBufferInfo();
+        return .{
+            .width = @intCast(info.srWindow.Right - info.srWindow.Left + 1),
+            .height = @intCast(info.srWindow.Bottom - info.srWindow.Top + 1),
+        };
     } else {
         var buffer: std.posix.system.winsize = undefined;
         if (std.posix.errno(std.posix.system.ioctl(std.io.getStdOut().handle, std.posix.T.IOCGWINSZ, @intFromPtr(&buffer))) == .SUCCESS) {
@@ -23,7 +20,46 @@ pub fn getTerminalSize() !TerminalSize {
                 .width = buffer.ws_col,
                 .height = buffer.ws_row,
             };
+        } else {
+            return error.Unexpected;
         }
+    }
+}
+
+pub const Position = struct {
+    x: u16 = 0,
+    y: u16 = 0,
+};
+
+pub fn getCursor() !Position {
+    if (builtin.os.tag == .windows) {
+        const info = try getConsoleScreenBufferInfo();
+        return .{
+            .x = @intCast(info.dwCursorPosition.X - info.srWindow.Left),
+            .y = @intCast(info.dwCursorPosition.Y - info.srWindow.Top),
+        };
+    }
+}
+
+pub fn setCursor(position: Position) !void {
+    if (builtin.os.tag == .windows) {
+        var info = try getConsoleScreenBufferInfo();
+        //info.dwCursorPosition.X = @intCast(position.x + @as(u16, @intCast(info.srWindow.Left)));
+        //info.dwCursorPosition.Y = @intCast(position.y + @as(u16, @intCast(info.srWindow.Top)));
+        info.dwCursorPosition.X = @as(i16, @intCast(position.x)) + info.srWindow.Left;
+        info.dwCursorPosition.Y = @as(i16, @intCast(position.y)) + info.srWindow.Top;
+        _ = try std.io.getStdOut().writer().print(">>{any}\n", .{info.srWindow});
+        _ = try std.io.getStdOut().writer().print(">>{any}\n", .{info.dwCursorPosition});
+        if (std.os.windows.kernel32.SetConsoleCursorPosition(std.io.getStdOut().handle, info.dwCursorPosition) != std.os.windows.TRUE) {
+            return error.Unexpected;
+        }
+    }
+}
+
+var consoleScreenBufferInfo: std.os.windows.CONSOLE_SCREEN_BUFFER_INFO = undefined;
+fn getConsoleScreenBufferInfo() !std.os.windows.CONSOLE_SCREEN_BUFFER_INFO {
+    if (std.os.windows.kernel32.GetConsoleScreenBufferInfo(std.io.getStdOut().handle, &consoleScreenBufferInfo) == std.os.windows.TRUE) {
+        return consoleScreenBufferInfo;
     }
     return error.Unexpected;
 }
